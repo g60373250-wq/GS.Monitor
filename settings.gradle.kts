d833a2159933792cad217bb220ef38354295f9d2
george <g60373250@gmail.com>
2026-06-13 19:52:46 +0900

    Add files via upload

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://jetbrains.space")
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://jetbrains.space")
+    }
+}
+
+rootProject.name = "GS.Monitor"
